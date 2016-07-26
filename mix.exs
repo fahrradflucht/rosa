c537646537a7c559e8606b03dbe8035c597ff96b
Fahrradflucht <mail@mathiswiehl.de>
2016-07-26 12:23:07 +0200

    Add travis (and bump elixir version to 1.3)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rosa.Mixfile do
   def project do
     [app: :rosa,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
