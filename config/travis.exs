972ab779fafca74b149d4122a749befa75ec3a08
Fahrradflucht <mail@mathiswiehl.de>
2016-07-26 12:30:30 +0200

    Fix failing postgres setup

--- config/travis.exs
+++ config/travis.exs
@@ -17,7 +17,7 @@ config :rosa, Rosa.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
-  database: "rosa_test",
+  database: "travis_rosa_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
