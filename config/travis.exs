426523abc3003bca0ca22f725d676eb258a86204
Mathis <mail@mathiswiehl.de>
2016-07-26 13:24:49 +0200

    Add travis (#5)
    We don't have any important tests at the moment but setting up travis early confirms that you don't go deep into the "works on my machine" weeds.
    
    I also bumped the Elixir version to 1.3 in mix.exs. I was using it anyway and the tests are passing ;).

---
+++ config/travis.exs
@@ -0,0 +1,26 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :rosa, Rosa.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Reduce the number of bcrypt rounds during tests
+config :comeonin, :bcrypt_log_rounds, 4
+
+# Configure your database
+config :rosa, Rosa.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "rosa_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# Configures Guardian secret
+config :guardian, Guardian,
+  secret_key: "nGPGtmC5jkDFGqZxN9XTJQRS8bKGBpmtNNO13Bb027AP0erSpx8EufFvDdKy"
