6ee96b601b5235a8bdd9cf70f190d9b08789c6ef
Fahrradflucht <mail@mathiswiehl.de>
2016-07-27 16:35:30 +0200

    Setup frontend test env.

---
+++ web/static/js/test/mocha.test.js
@@ -0,0 +1,8 @@
+import expect from 'expect';
+import * as actions from '../actions/shell'; 
+
+describe('shell actions', () => {
+    it('should create an action to toggle the sidenav', () => {
+        expect(actions.toggleSidenav()).toEqual({type: 'TOGGLE_SIDENAV'});
+    })
+})
