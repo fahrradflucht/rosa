a4811fd0b6154a5c012273ee548eef73162ad286
Fahrradflucht <mail@mathiswiehl.de>
2016-07-29 13:59:42 +0200

    Add tests for reducers and actions
    The tests that handle the error cases of session management don't work at the moment. There is an [unaswered Stackoverflow question](http://stackoverflow.com/questions/38641979/why-does-redux-mock-store-dont-show-an-action-dispatched-in-catch-promises) related to this. It skipped them for now.

---
+++ web/static/js/test/reducers/session.spec.js
@@ -0,0 +1,53 @@
+import expect from 'expect';
+import session from '../../reducers/session';
+import { fakeUser } from '../helpers';
+
+describe('session reducer', () => {
+    it('should have an initial state', () => {
+        expect(session(undefined, '')).toExist();
+    })
+
+    it('should set isFetching: true on REQUEST_SESSION', () => {
+        expect(session(undefined, { type: 'REQUEST_SESSION' }).isFetching)
+        .toEqual(true);
+    });
+    
+    it('should set isFetching: false on SET_SESSION_USER', () => {
+        const initialState = Object.assign(session(undefined, ''), {
+            isFetching: true
+        });
+        expect(session(initialState, { type: 'SET_SESSION_USER' }).isFetching)
+        .toEqual(false);
+    });
+
+    it('should set the user on SET_SESSION_USER', () => {
+        expect(session(undefined, {
+            type: 'SET_SESSION_USER',
+            user: fakeUser
+        }).user).toEqual(fakeUser)
+    });
+
+    it('should set isFetching: false on SET_SESSION_ERROR', () => {
+        const initialState = Object.assign(session(undefined, ''), {
+            isFetching: true
+        });
+        expect(session(initialState, { type: 'SET_SESSION_ERROR' }).isFetching)
+        .toEqual(false);
+    });
+
+    it('should set the error on SET_SESSION_ERROR', () => {
+        expect(session(undefined, {
+            type: 'SET_SESSION_ERROR',
+            error: new Error('Not Authenticated')
+        }).error).toEqual(new Error('Not Authenticated'))
+    });
+
+    it('should reset the state on DELETE_SESSION', () => {
+        const initialState = Object.assign(session(undefined, ''), {
+            user: fakeUser
+        });
+        expect(session(initialState, { type: 'DELETE_SESSION' })).toEqual(
+            session(undefined, '')
+        )
+    });
+})
---
+++ web/static/js/test/reducers/shell.spec.js
@@ -0,0 +1,24 @@
+import expect from 'expect';
+import shell from '../../reducers/shell'; 
+
+describe('shell reducer', () => {
+    it('should have an initial state', () => {
+        expect(shell(undefined, '')).toExist();
+    })
+    
+    it('toggle the toggled state on TOGGLE_SIDENAV', () => {
+        expect(shell({toggled: false}, { type: 'TOGGLE_SIDENAV' })).toEqual({
+            toggled: true
+        });
+        expect(shell({toggled: true}, { type: 'TOGGLE_SIDENAV' })).toEqual({
+            toggled: false
+        })
+    });
+    
+    it('should not toggle the state on other actions', () => {
+        expect(shell({toggled: false}, { type: 'TEST' })).toEqual({
+            toggled: false
+        });
+    })
+    
+})
