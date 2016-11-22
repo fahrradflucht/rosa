12efb03a4b60c819b2af010a8bf818516d619d61
Fahrradflucht <mail@mathiswiehl.de>
2016-11-22 14:46:34 +0100

    Update guardian to 0.13.0

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Rosa.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:comeonin, "~> 2.5"},
-     {:guardian, "~> 0.12.0"},
+     {:guardian, "~> 0.13.0"},
      {:hound, "~> 1.0"}]
   end
 
